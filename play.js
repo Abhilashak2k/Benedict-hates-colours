76797142a50147c03c0d4959d3ee6544796f9a5e
Abhilasha Kumari <31110940+Abhilashak2k@users.noreply.github.com>
2020-01-19 11:55:33 +0530

    Not first commit

--- play.js
+++ play.js
@@ -3,7 +3,7 @@
 	function handleUpdate() {
 		const suffix = this.dataset.sizing || '';
 		// to select the extension to add because a number itself is not a unit to change the blur. I've used data-sizing as a user defined attribute which will be retrived by this.data.sizing.
-		//also since hex code is anough for the colour, no need t append anything there, But it should also not be null.
+		//also since hex code is enough for the colour, no need t append anything there, But it should also not be null.
 		document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 	}
 	inputs.forEach(i => i.addEventListener('change', handleUpdate));
